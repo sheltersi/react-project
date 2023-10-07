6838c214b08d3b3d6d6731f571d907fa9a1af683
sheltersi <sheltersibanda002@gmail>
2023-10-07 13:54:39 +0200

    another commit

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -26,38 +26,54 @@ function Contact(){
 
     return(
         <>
-         <h1 className="h1-contact text-4xl pb-6 pt-6 font-extrabold font-Young-serif text-slate-200">
+         <h1 className="h1-contact text-2xl md:text-4xl pb-6 pt-6 font-extrabold font-Young-serif text-slate-200">
          Get in Touch with us</h1>
-        <div className="contact">
+        <div className="contact md:flex block">
         
        
-        <div className="picture-container">
-        <img src={Picture} alt="electricity-installation" className="picture"/>
-        <h3>Talk to our support Team find out how we can help you</h3>
-        <hr className="hr" />
+        <div className="picture-container border-y-slate-600">
+        {/* <img src={Picture} alt="electricity-installation" className="picture"/> */}
+        <h3 className="text-xl font-bold text-red-600 md:text-white md:3xl md:font-extrabold">Talk to our support Team find out how we can help you</h3>
+        <hr className="hr pb-4 " />
         <p>Fill out your information and an Odims Engineering representative will 
         reach out to you. Have a simple question? Check out our FAQ.</p>
         </div>
 
 
-        <div className="form mx-1">
-        <div class="grid grid-cols-2 gap-8">
-  
-        <div >
-<TextField id="outlined-basic" label="Name" variant="outlined" className="textfield" />
-</div>
-<div>
-<TextField id="outlined-basic" label="Surname" variant="outlined" className="textfield"/>
-</div>
-
-<div>
-     <TextField id="outlined-basic" label="Email Address" variant="outlined" type="email" className="textfield"/>
-     </div>
-     <div>
- <TextField id="outlined-basic" label="Phone Number" variant="outlined" type="cell number" max="10" className="textfield"/>
-</div>
+        <div className="form px-2 py-2 w-auto md:h-auto space-y-8">
 
 
+        <div class="order-1 space-y-5 space-x-1 gap-4 md:grid md:grid-cols-2 md:gap-8 items-center">
+        <div class="mb-4">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="name">
+   Name
+      </label>
+      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700
+       leading-tight focus:outline-none focus:shadow-outline" id="name" type="text" placeholder="name" />
+    </div>
+    <div class="mb-4">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="Surname">
+      Surname
+      </label>
+      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700
+       leading-tight focus:outline-none focus:shadow-outline" id="surname" type="text" placeholder="surname"/>
+    </div>
+    <div class="mb-4">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
+  Email
+      </label>
+      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight 
+      focus:outline-none focus:shadow-outline" id="email" type="text" placeholder="email"/>
+    </div>
+    <div class="mb-4">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="phone-number">
+      Phone Number
+      </label>
+      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 
+      leading-tight focus:outline-none focus:shadow-outline" id="phone-number" type="text" placeholder="phone-number"/>
+    </div>
+
+{/* 
 <div>
   <FormControl sx={{ m: 1, minWidth: 80 }}>
     <InputLabel id="demo-simple-select-autowidth-label">Province</InputLabel>
@@ -77,11 +93,11 @@ function Contact(){
       <MenuItem value={22}>Twenty one and a half</MenuItem>
     </Select>
   </FormControl>
-</div> 
+</div>  */}
 
 
 
-<div>
+{/* <div>
   <FormControl sx={{ m: 1, minWidth: 80 }}>
     <InputLabel id="demo-simple-select-autowidth-label">City</InputLabel>
     <Select
@@ -100,15 +116,51 @@ function Contact(){
       <MenuItem value={22}>Twenty one and a half</MenuItem>
     </Select>
   </FormControl>
-</div> 
+</div>  */}
+
+<div >
+<div class="w-full md:w-1/9 px-3 mb-6 md:mb-0">
+      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
+      Province
+      </label>
+
+<div class="relative text-lg">
+        <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+          <option>New Mexico</option>
+          <option>Missouri</option>
+          <option>Texas</option>
+        </select>
+        <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+          <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+        </div>
+      </div>
+    </div>
+    </div>
 
 
+<div>
+<div class="w-full md:w-1/9 px-3 mb-6 md:mb-0">
+      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
+        City
+      </label>
+      <div class="relative text-lg">
+        <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+          <option>New Mexico</option>
+          <option>Missouri</option>
+          <option>Texas</option>
+        </select>
+        <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+          <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+        </div>
+      </div>
+    </div>
+</div>
 
 <div className="col-span-2 "> 
-<p>What Would you like to discuss?</p>
+<p className="text-lg">What Would you like to discuss?</p>
 <textarea
 rows={6} className="
-placeholder:text-slate-400 block bg-white w-full
+placeholder:text-slate-400 block bg-white w-11/12
 border border-slate-300 rounded-md shadow-sm focus:outline-none
 focus:ring-sky-500 focus:ring-1" placeholder="  Ask Anything .............. "
 name="ask-txt"
@@ -120,7 +172,8 @@ name="ask-txt"
 
 <div className="col-span-2 "> <input type="checkbox" name="mycheckbox"
     className="block bg-white border border-slate-300"
-/>By checking the box and clicking "Submit", you are agreeing to Odims Engineering Privacy Statement
+/>
+<p className="text-lg">By checking the box and clicking "Submit", you are agreeing to Odims Engineering Privacy Statement</p>
 </div>
 
 <div className="col-span-2 "> 
@@ -137,6 +190,7 @@ Submit
         </div>
         </div>
         </div>
+     
         <div>
         <Footer/>
         </div>
