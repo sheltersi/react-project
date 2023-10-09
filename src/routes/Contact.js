93c9c39332138d8d7849a684aeeb0b11520de05c
sheltersi <sheltersibanda002@gmail>
2023-10-09 14:56:45 +0200

    commit

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -120,24 +120,21 @@ function Contact(){
   </FormControl>
 </div>  */}
 
-<div >
-<div class="w-full md:w-1/9 px-3 mb-6 md:mb-0">
+<div>  <div class="w-full md:w-4/5 px-0 mb-6 md:mb-0">
       <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
-      Province
+            Province
       </label>
-
-<div class="relative text-lg">
+      <div class="relative">
         <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
-          <option>New Mexico</option>
-          <option>Missouri</option>
-          <option>Texas</option>
+          <option>Gauteng</option>
+          <option>North-West</option>
+          <option>Free State</option>
         </select>
         <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
           <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
         </div>
       </div>
-    </div>
-    </div>
+    </div></div>
 
 
 <div>
@@ -147,9 +144,14 @@ function Contact(){
       </label>
       <div class="relative text-lg">
         <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
-          <option>New Mexico</option>
-          <option>Missouri</option>
-          <option>Texas</option>
+        <option>Johannesburg</option>
+          <option>Pretoria</option>
+          <option>Alberton</option>
+          <option>Edenvale</option>
+          <option>Mogale</option>
+          <option>Saulsville</option>
+          <option>Centurion</option>
+          <option>Ekurhuleni</option>
         </select>
         <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
           <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
--- src/routes/Services.js
+++ src/routes/Services.js
@@ -7,6 +7,7 @@ import Image2 from '../assets/electrical-services-IMG/access-control.jpg'
 import Image1 from '../assets/electrical-services-IMG/electricians-1.jpg'
 import geyser2 from '../assets/electrical-services-IMG/geyser2.jpg'
 import GateM from '../assets/electrical-services-IMG/choosing-gate-motor.jpg'
+import { BsSendFill } from "react-icons/bs"
 
 
 
@@ -47,7 +48,7 @@ function Services(){
 </div>
 
 <h1 className="text-4xl text-orange-400 mt-20 font-extrabold">Electrical Solutions With Odims Engineering</h1>
-<p className="text-orange-200 text-sm pt-5">Odims Engineering a Company of professional Engineers with more than 10 years Experience</p>
+<p className="text-orange-200 text-sm pt-5">Odims Engineering is a Company of professional Engineers with more than 10 years of Experience</p>
 
 
 <div className=" flex justify-between ">
@@ -118,90 +119,131 @@ function Services(){
 <div className="bg-yellow-500 h-10 text-center pt-1 mt-16 text-xl font-extrabold text-slate-800">
 Book A Service With Us</div>
 
-<div className="pt-10 w-full bg-slate-500 ">
+<div className="pt-10 w-full bg-slate-400 pb-6 ">
 
-<div className="grid grid-cols-2 gap-5 ml-2">
-{/* First role */}
-
-<div class="mb-4">
+<div className="grid grid-cols-2 gap-3 ml-10">
+<div><div class="mb-4">
       <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
-        Username
+        First Name/ Names
       </label>
       <input class="shadow appearance-none border rounded w-4/5 py-2 px-3 text-gray-700 leading-tight 
-      focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username"/>
+      focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="first names...."/>
     </div>
-
-    <div class="mb-4 justify-end" >
-      <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
-        Username
+   </div>
+<div><div class="mb-4">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="surname">
+        Surname
       </label>
       <input class="shadow appearance-none border rounded w-4/5 py-2 px-3 text-gray-700 leading-tight 
-      focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username"/>
+      focus:outline-none focus:shadow-outline" id="surname" type="text" placeholder="Surname....."/>
     </div>
-
-
-
-    <div class="mb-4">
-      <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
-        Username
+   </div>
+<div><div class="mb-4">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
+       Email Address
       </label>
       <input class="shadow appearance-none border rounded w-4/5 py-2 px-3 text-gray-700 leading-tight 
-      focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username"/>
+      focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Enter your email address....."/>
     </div>
-
-    <div class="mb-4">
-      <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
-        Username
+   </div>
+<div><div class="mb-4">
+      <label class="block text-gray-700 text-sm font-bold mb-2" for="number">
+       Phone Number
       </label>
       <input class="shadow appearance-none border rounded w-4/5 py-2 px-3 text-gray-700 leading-tight 
-      focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username"/>
+      focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="(081) 000........."/>
     </div>
+   </div>
 
-</div>
-
-<div>
-    <div class="w-full md:w-3/5 px-2 mb-6 md:mb-0">
+<div>  <div class="w-full md:w-4/5 px-0 mb-6 md:mb-0">
       <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
-        State
+            Province
       </label>
       <div class="relative">
         <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
-          <option>New Mexico</option>
-          <option>Missouri</option>
-          <option>Texas</option>
+          <option>Gauteng</option>
+          <option>North-West</option>
+          <option>Free State</option>
+        </select>
+        <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+          <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+        </div>
+      </div>
+    </div></div>
+<div>  <div class="w-full md:w-4/5 px-0 mb-6 md:mb-0">
+      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
+        City
+      </label>
+      <div class="relative">
+        <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+          <option>Johannesburg</option>
+          <option>Pretoria</option>
+          <option>Alberton</option>
+          <option>Edenvale</option>
+          <option>Mogale</option>
+          <option>Saulsville</option>
+          <option>Centurion</option>
+          <option>Ekurhuleni</option>
+        </select>
+        <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+          <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+        </div>
+      </div>
+    </div></div>
+<div>
+<div class="w-full md:w-4/5 px-0 mb-6 md:mb-0">
+      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
+       Select Service
+      </label>
+      <div class="relative">
+        <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
+        <option>Electrical Fault Finding.</option>
+          <option>Full House, Office or Company Wiring.</option>
+          <option>Installation and Servicing of Garage Motor.</option>
+          <option> Electric Fencing and servicing.</option>
+          <option>Installation and Servicing Intercom System.</option>
+          <option>Access Control.</option>
+          <option>CCTV Installation and Servicing.</option>
+          <option>Installation and Servicing Alarm System.</option>
+          <option>Installation and Servicing Power Backups.</option>
+          <option>Installation and Servicing of Swimming Pool Pumps.</option>
+          <option>Installation and Servicing of Borehole Pumps.</option>
+          <option>Installation and Servicing of Geyser.</option>
         </select>
         <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
           <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
         </div>
       </div>
     </div>
-</div> 
-
-<div>
-    <div class="w-full md:w-4/5 px-2 mb-6 md:mb-0">
-      <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-state">
-        State
-      </label>
-      <div class="relative">
-        <select class="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 py-3 px-4 pr-8 rounded leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-state">
-          <option>New Mexico</option>
-          <option>Missouri</option>
-          <option>Texas</option>
-        </select>
-        <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
-          <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
-        </div>
-      </div>
-    </div>
 </div>
 
+<div className="col-span-2">
+    <p className="text-green-700">If you were not able to find the service you need please type the service below</p>
+    <textarea
+rows={6} className=" text-sm md:text-lg text-black
+placeholder:text-slate-400 block bg-white w-11/12
+border border-slate-300 rounded-md shadow-sm focus:outline-none
+focus:ring-sky-500 focus:ring-1" placeholder="Enter the services .............. "
+name="ask-txt"
+/>
 </div>
 
+<div className="col-span-2 justify-end text-right mr-20 "> 
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full">
+<p className="text-sm md:text-lg flex">Submit <BsSendFill className="mt-2 ml-2"/> </p>
+</button>
+</div>   
 
+</div>
 
 
 
 </div>
+
+
+
+</div>
+
 
 </div>
 
